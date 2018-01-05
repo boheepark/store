b5b7afb8691fb2ddb8249e03fad41484b615814f
Bohee Park <boheepark91@gmail.com>
2018-01-05 11:41:30 -0500

    profile test

--- e2e/profile.test.js
+++ e2e/profile.test.js
@@ -1,52 +1,50 @@
 import {Selector} from 'testcafe';
+
 import randomstring from 'randomstring';
-// const randomstring = require('randomstring');
 
-
-const USERNAME = randomstring.generate();
-const EMAIL = `${USERNAME}@email.com`;
-const PASSWORD = 'greaterthanten';
-const DATE = new Date();
-
+const username = randomstring.generate();
+const email = `${username}@email.com`;
+const password = 'greaterthanten';
+const currentDate = new Date();
 
 const TEST_URL = process.env.TEST_URL;
+
+
 fixture('/profile').page(`${TEST_URL}/profile`);
 
 
 test(`Should not display profile if the user is not signed in.`, async (t) => {
-  await t
-    .navigateTo(`${TEST_URL}/profile`)
-    .expect(Selector('p').withText(
-      'You must be signed in to view this page.'
-    ).exists).ok()
-    .expect(Selector('a').withText('Profile').exists).notOk()
-    .expect(Selector('a').withText('Signout').exists).notOk()
-    .expect(Selector('a').withText('Signup').exists).ok()
-    .expect(Selector('a').withText('Signin').exists).ok();
+    await t
+        .navigateTo(`${TEST_URL}/profile`)
+        .expect(Selector('p').withText('You must be signed in to view this page.').exists).ok()
+        .expect(Selector('a').withText('Profile').exists).notOk()
+        .expect(Selector('a').withText('Sign Out').exists).notOk()
+        .expect(Selector('a').withText('Sign Up').exists).ok()
+        .expect(Selector('a').withText('Sign In').exists).ok();
 });
 
 
 test(`Should display profile if the user is signed in.`, async (t) => {
 
-  // signup user
-  await t
-    .navigateTo(`${TEST_URL}/signup`)
-    .typeText('input[name="username"]', USERNAME)
-    .typeText('input[name="email"]', EMAIL)
-    .typeText('input[name="password"]', PASSWORD)
-    .click(Selector('input[type="submit"]'));
+    // signup user
+    await t
+        .navigateTo(`${TEST_URL}/signup`)
+        .typeText('input[name="username"]', username)
+        .typeText('input[name="email"]', email)
+        .typeText('input[name="password"]', password)
+        .click(Selector('input[type="submit"]'));
 
-  // assert '/profile' is displayed properly
-  await t
-    .navigateTo(`${TEST_URL}/profile`)
-    .expect(Selector('li > strong').withText('User ID:').exists).ok()
-    .expect(Selector('li > strong').withText('Email:').exists).ok()
-    .expect(Selector('li').withText(EMAIL).exists).ok()
-    .expect(Selector('li > strong').withText('Username:').exists).ok()
-    .expect(Selector('li').withText(USERNAME).exists).ok()
-    .expect(Selector('a').withText('Profile').exists).ok()
-    .expect(Selector('a').withText('Signout').exists).ok()
-    .expect(Selector('a').withText('Signup').exists).notOk()
-    .expect(Selector('a').withText('Signin').exists).notOk();
+    // assert '/profile' is displayed properly
+    await t
+        .navigateTo(`${TEST_URL}/profile`)
+        .expect(Selector('li > strong').withText('User ID:').exists).ok()
+        .expect(Selector('li > strong').withText('Email:').exists).ok()
+        .expect(Selector('li').withText(email).exists).ok()
+        .expect(Selector('li > strong').withText('Username:').exists).ok()
+        .expect(Selector('li').withText(username).exists).ok()
+        .expect(Selector('a').withText('Profile').exists).ok()
+        .expect(Selector('a').withText('Sign Out').exists).ok()
+        .expect(Selector('a').withText('Sign Up').exists).notOk()
+        .expect(Selector('a').withText('Sign In').exists).notOk();
 
 });
