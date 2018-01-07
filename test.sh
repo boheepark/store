b56eb0aef4f61333e4979c8e78bfce169e0a3b78
Bohee Park <boheepark91@gmail.com>
2018-01-06 20:35:24 -0500

    test

--- test.sh
+++ test.sh
@@ -1,21 +1,63 @@
 #!/bin/bash
 
-fails=''
+
+if [ -z ${env} ];
+then
+  env=$1
+fi
+
+file=""
+fails=""
+
+if [[ "${env}" == "dev" ]];
+then
+  file="docker-compose-dev.yml"
+elif [[ "${env}" == "stage" ]];
+then
+  file="docker-compose-stage.yml"
+elif [[ "${env}" == "prod" ]];
+then
+  file="docker-compose-prod.yml"
+else
+  echo "USAGE: sh test.sh environment_name"
+  echo "* environment_name: dev, stage, or prod"
+  exit 1
+fi
 
 inspect() {
-  if [ $1 -ne 0 ]; then
+  if [ $1 -ne 0 ];
+  then
     fails="${fails} $2"
   fi
 }
 
-docker-compose run users-service python manage.py cov
+/bin/sleep 5
+
+# docker-compose -f docker-compose-ci.yml run users-service flask recreate_db
+# docker-compose -f docker-compose-ci.yml run users-service flask seed_db
+docker-compose -f $file run users-service flask recreate_db
+docker-compose -f $file run users-service flask seed_db
+
+# docker-compose -f docker-compose-ci.yml run users-service flask test --coverage
+docker-compose -f $file run users-service flask test --coverage
 inspect $? users-service
 
-testcafe chrome e2e
-inspect $? e2e
+docker-compose -f $file run users-service flake8 project
+inspect $? users-lint
 
-testcafe firefox e2e
-inspect $? e2e
+
+if [[ "${env}" == "stage" ]];
+then
+  docker-compose -f $file run client yarn test --verbose --coverage
+  inspect $? client
+
+  testcafe chrome e2e
+  inspect $? e2e_chrome
+else
+  testcafe chrome e2e/index.test.js
+  inspect $? e2e
+fi
+
 
 if [ -n "${fails}" ];
   then
