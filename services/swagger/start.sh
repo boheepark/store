5bbe8d97b7d3d0b58b1b5f906e03fcd938cf8323
Bohee Park <boheepark91@gmail.com>
2018-01-07 02:01:10 -0500

    start

---
+++ services/swagger/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+if [ $URL != "**None**" ];
+then
+  sed -i -e 's@http://petstore.swagger.io/v2/swagger.json@'"$URL"'@g' /usr/share/nginx/html/index.html
+fi
+
+
+exec nginx -g 'daemon off;'
